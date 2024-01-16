a65c770e71ad0a6cb8d1d7029b26b981c5a5859a
namoong0917 <namoong0917@gmail.com>
2024-01-17 03:38:07 +0900

    라우팅 및 컴퍼넌트 세팅

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import GlobalStyle from "./assets/reset/GlobalStyle";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./assets/scss/main.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
+    <GlobalStyle />
     <React.StrictMode>
       <App />
     </React.StrictMode>
---
+++ src/views/HomeView.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Skip from "../compoenets/Skip";
+import Header from "../compoenets/Header";
+import Intro from "../compoenets/Intro";
+import Skill from "../compoenets/Skill";
+import Site from "./../compoenets/Site";
+import Port from "../compoenets/Port";
+import Contact from "../compoenets/Contact";
+import Footer from "../compoenets/Footer";
+import Main from "./../compoenets/Main";
+
+const HomeView = () => {
+  return (
+    <>
+      <Skip />
+      <Header />
+      <Main>
+        <Intro />
+        <Skill />
+        <Site />
+        <Port />
+        <Contact />
+      </Main>
+
+      <Footer />
+    </>
+  );
+};
+
+export default HomeView;
