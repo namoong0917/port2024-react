180d505a68d36d6077962a7d0f240305539432d2
namoong0917 <namoong0917@gmail.com>
2024-01-18 03:31:35 +0900

    연락처 완료

--- src/compoenets/Contact.jsx
+++ src/compoenets/Contact.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
+const contactText = [
+  {
+    link: "#",
+    title: "KAKAO : milktree123",
+  },
+  {
+    link: "https://mail.google.com/mail/?view=cm&amp;fs=1&amp;to=namoong0917@gmail.com",
+
+    title: "mail : namoong0917@GMAIL.COM",
+  },
+];
+
 const contact = () => {
-  return <div>contact</div>;
+  return (
+    <section id="contact">
+      <div className="contact_inner">
+        <h2 className="contact_title">Contact</h2>
+        <div className="contact_text">
+          <div className="text">
+            {contactText.map((contact, key) => (
+              <div key={key}>
+                <a
+                  href={contact.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {contact.title}
+                </a>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="contact_lines bottom" aria-hidden="true">
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+          <span className="line"></span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default contact;
